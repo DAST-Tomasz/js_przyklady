2c420dfd623bd54b775cd199e92e2e4742b1cc05
Tomasz W <tomasz.wasko@dast.com.pl>
2017-04-02 08:47:15 +0200

    ostatnie zmiany

--- js/14-json.js
+++ js/14-json.js
@@ -2,43 +2,79 @@
 
 var jsonOsoby = {
     "osoby": [
-        {imie: "Krystian", nazwisko: "Dziopa", wzrost: "180", oczy: "niebieskie", zainteresowania: [ {nazwa: "podroze"}, {nazwa: "gotowanie"}] },
-        {imie: "Kasia", nazwisko: "Zając", wzrost: "170", oczy: "zielone"},
-        {imie: "Łukasz", nazwisko: "Badocha", wzrost: "170", oczy: "duże"}
+        {
+            imie: "Krystian",
+            nazwisko: "Dziopa",
+            wzrost: "180",
+            oczy: "niebieskie",
+            zainteresowania: [{
+                nazwa: "podroze"
+            }, {
+                nazwa: "gotowanie"
+            }]
+        },
+        {
+            imie: "Kasia",
+            nazwisko: "Zając",
+            wzrost: "170",
+            oczy: "zielone"
+        },
+        {
+            imie: "Łukasz",
+            nazwisko: "Badocha",
+            wzrost: "170",
+            oczy: "duże"
+        }
     ]
 }
 
 
-jsonOsoby.osoby.forEach(function(element, index){
-    
+jsonOsoby.osoby.forEach(function (element, index) {
 
-    console.log("Pracownik " + (index+1) + ' ma na imie ' + element.imie);
-    
+
+    console.log("Pracownik " + (index + 1) + ' ma na imie ' + element.imie);
+
 })
 
 var jsonPracownicy = {
-    "pracownicy":[
-        {"firstName": "Krystian", "lastName": "Dziopa"},
-        {"firstName": "Anna", "lastName": "Szapiel"},
-        {"firstName": "Piotr", "lastName": "Żmuda"}
+    "pracownicy": [
+        {
+            "firstName": "Krystian",
+            "lastName": "Dziopa"
+        },
+        {
+            "firstName": "Anna",
+            "lastName": "Szapiel"
+        },
+        {
+            "firstName": "Piotr",
+            "lastName": "Żmuda"
+        }
     ]
 }
 
-jsonPracownicy.pracownicy.forEach(function(element, index){
-    
+jsonPracownicy.pracownicy.forEach(function (element, index) {
+
     console.log((index) + " - " + element.firstName + " " + element.lastName);
 })
 
-var jsonLiczby = {
-    "liczby": [
-        {parzysta: "2, 4, 6, 8, 10"},
-        {nieparzysta: "1, 3, 5, 7, 9"}
-    ]
-}
+// tablica
+var liczby = [];
 
-jsonLiczby.liczby.forEach(function(element, index){
+for  (var j = 0; j<=100; j++) {
+    var rand = Math.floor(Math.random() * 100);
+    liczby.push(rand);
+}
+//var liczby = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// petla ktora wypluje liczby parzyste i nieparzyste
+for (var i = 0; i < liczby.length; i++) {
+    if (liczby[i] % 2 == 0) {
+        console.log(liczby[i] + ' jest parzysta');
+    } else {
+        console.log(liczby[i] + ' jest nieparzysta');
+    }
     
-    
-    
-})
+    if (i == 10) break;
+}
 
